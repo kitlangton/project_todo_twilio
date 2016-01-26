9186376a464078d0c0c8ad8b0550f9d1085b8947
koziscool <kozjob@yahoo.com>
2016-01-26 15:51:39 -0600

    texting works

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,11 @@
 class TextsController < ApplicationController
   def send_text
+    client = Twilio::REST::Client.new
+    todo = Todo.find( params[ :id ] )
+    # client.messages.create( from, to, body )
+    client.messages.create( from: ENV[ 'from_kit_number' ], to: ENV[ 'to_john_number' ], body: todo.task )
     puts params[:id]
     redirect_to todos_path
   end
 end
+
