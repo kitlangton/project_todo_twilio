991e89260e6d8ba2efb690e76398827ee91c89b7
koziscool <kozjob@yahoo.com>
2016-01-26 13:27:20 -0600

    through story 4

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,47 @@
 class TodosController < ApplicationController
   def index
     @todos = Todo.all.order(:completion_date)
+    @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new( todo_params )
+    if @todo.save
+      redirect_to todos_path
+    else
+      redirect_to todos_path
+    end
+  end
+
+  def edit
+    @todo = Todo.find( params[ :id ] )
+  end
+
+  def update
+    @todo = Todo.find( params[ :id ])
+    if @todo.update( todo_params )
+      redirect_to todos_path
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @todo = Todo.find( params[ :id ] )
+    if @todo.destroy
+      redirect_to todos_path
+    else
+      redirect_to todos_path
+    end
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit( :task, :completion_date )
+  end
+
+
+
 end
+
