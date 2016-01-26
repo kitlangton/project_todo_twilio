0833507d3ceccf50851e451418ce44e4b148ced4
Kit Langton <kitlangton@gmail.com>
2016-01-26 14:34:14 -0500

    Added flashes

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,47 +1,50 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:destroy, :edit, :update]
+
   def index
     @todos = Todo.all.order(:completion_date)
     @todo = Todo.new
   end
 
   def create
+    @todos = Todo.all.order(:completion_date)
     @todo = Todo.new( todo_params )
     if @todo.save
-      redirect_to todos_path
+      redirect_to todos_path, notice: "Created todo!"
     else
-      redirect_to todos_path
+      flash.now[:alert] = "Couldn't create."
+      render :index
     end
   end
 
   def edit
-    @todo = Todo.find( params[ :id ] )
   end
 
   def update
-    @todo = Todo.find( params[ :id ])
     if @todo.update( todo_params )
-      redirect_to todos_path
+      redirect_to todos_path, notice: "Updated todo!"
     else
+      flash.now[:alert] = "Couldn't update."
       render :edit
     end
   end
 
-
   def destroy
-    @todo = Todo.find( params[ :id ] )
     if @todo.destroy
-      redirect_to todos_path
+      redirect_to todos_path, notice: "Destroyed todo!"
     else
-      redirect_to todos_path
+      redirect_to todos_path, alert: "Could not destroy."
     end
   end
 
   private
+
+  def set_todo
+    @todo = Todo.find( params[ :id ] )
+  end
+
   def todo_params
     params.require(:todo).permit( :task, :completion_date )
   end
-
-
-
 end
 
